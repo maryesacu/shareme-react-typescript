dd67c98f3dc05790e899a3babc5d82180907e8c6
marlon <maryesacu@gmail.com>
2022-10-30 15:50:17 -0500

    first commit

---
+++ shareme_frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+/* import Login from './components/Login'
+import Home from './containers/Home' */
+
+const App = (): JSX.Element =>
+{
+    return (
+      <div>
+        <h1>
+          Hello World.
+        </h1>
+      </div>
+/*         <Routes>
+            <Route path='login' element={<Login />}></Route>
+            <Route path='/*' element={<Home />}></Route>
+        </Routes> */
+    )
+}
+
+export default App
